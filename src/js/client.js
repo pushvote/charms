00412fc0eebc54d0341edfc15f333003e6c78311
jnull <admin@jarednull.com>
2017-02-20 06:31:20 -0800

    tet

---
+++ src/js/client.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom"
+class Layout extends React.Component {
+  render() {
+    return (
+      <h1>It Works66666666666666666 Wooop!</h1>
+    );
+  }
+}
+
+
+app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
