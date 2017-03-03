5fa1e65da992da25bcb06cab2303ab111ae3bd6e
jnull <admin@jarednull.com>
2017-03-03 08:57:40 -0800

    Upaded src/index.html

--- src/js/client.js
+++ src/js/client.js
@@ -2,16 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom"
 import Layout from "./components/Layout";
 
+import Archives from "./components/Archives";
+import Featured from "./components/Featured";
+import Settings from "./components/Settings";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-const app = document.querySelector('#app');
-ReactDOM.render(
 
-  <Router history={hashHistory}>
-    <Route path="/" component={Layout}>
-      <IndexRoute component={Featured}></IndexRoute>
-      <Route path="archives(/:article)" name="archives" component={Archives}></Route>
-      <Route path="settings" name="settings" component={Settings}></Route>
-    </Route>
-  </Router>
+const app = document.querySelector('#app');
+$('[data-toggle="popover"]').popover();
+
+ReactDOM.render(
+<Layout/>
+/*
+<Router history={hashHistory}>
+  <Route path="/" component={Layout}>
+    <IndexRoute component={Featured}></IndexRoute>
+    <Route path="archives(/:article)" name="archives" component={Archives}></Route>
+    <Route path="settings" name="settings" component={Settings}></Route>
+  </Route>
+</Router>
+*/
+
   , app);
