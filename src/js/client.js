1454b68cbb09a9018f27e026835b26ae05fb72ea
jnull <admin@jarednull.com>
2017-03-04 22:24:44 -0800

    Added Badges to ResultsTables

--- src/js/client.js
+++ src/js/client.js
@@ -11,6 +11,18 @@ import { Router, Route, IndexRoute, hashHistory } from "react-router";
 const app = document.querySelector('#app');
 $('[data-toggle="popover"]').popover();
 
+
+//http://stackoverflow.com/questions/13413057/how-to-insert-close-button-in-popover-for-bootstrap
+$(document).ready(function() {
+    $("[data-toggle='popover']").popover({
+        placement: 'bottom',
+        html: 'true',
+        title : '<span class="text-info"><strong>title</strong></span>'+
+                '<button type="button" id="close" class="close" onclick="$(&quot;#example&quot;).popover(&quot;hide&quot;);">&times;</button>',
+        content : 'test'
+    });
+});
+
 ReactDOM.render(
 <Layout/>
 /*
